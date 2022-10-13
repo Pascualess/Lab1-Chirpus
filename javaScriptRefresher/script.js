372b897110c4c01123f4a1e0cccbb183811adcb2
Jacob Pascua <pascuajakecreed@gmail.com>
2022-10-12 22:31:57 -0400

    java script refresher lab finished

---
+++ javaScriptRefresher/script.js
@@ -0,0 +1,20 @@
+let firstName = "Jacob";
+let lastName = "Pascua";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+let population = 672351;
+
+console.log(population);
+
+population = population * 3;
+
+console.log(population);
+
+let javaScriptIsCool = true;
+
+let colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Purple"];
+
+for (i = 1; i <= 5; i++) console.log(i);
+for (i = 0; i <= 4; i++) console.log(i);
